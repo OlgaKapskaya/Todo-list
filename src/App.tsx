a57a7ad92745900c1460769a446eda48f4f86624
OlgaKapskaya <olikbuko@gmail.com>
2022-09-19 21:37:04 +0300

    lesson 2 .map .filter

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import {TodolistComponent} from "./component/TodolistComponent";
 function App() {
 
     const title1 = "What to learn1";
-    const title2 = "What to learn2"
+    const title2 = "What to learn2";
     const tasks1 = [
         {id: 1, title: "HTML&CSS", isDone: true, newValue: true},
         {id: 2, title: "JS", isDone: true, newValue: true},
         {id: 3, title: "ReactJS", isDone: false, newValue: true}
-    ]
+    ];
     const tasks2 = [
         {id: 1, title: "Hello world", isDone: true},
         {id: 2, title: "I am Happy", isDone: false},
         {id: 3, title: "Yo", isDone: false}
-    ]
+    ];
 
 
     return (
