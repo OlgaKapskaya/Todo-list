a093a74cd00f3baec489a9ebfdaed7b447d2c2d2
OlgaKapskaya <olikbuko@gmail.com>
2022-10-15 13:23:40 +0300

    Updates

---
+++ precache-manifest.e4e42c778aeb375ffd0d6ada3a9f1b48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1909b08ddb7d1d9bd1cdf6b4b09d25ce",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "56192245758b81b811dc",
+    "url": "/Todo-list/static/css/main.e1f68439.chunk.css"
+  },
+  {
+    "revision": "d4385ee83e572f6ffcc7",
+    "url": "/Todo-list/static/js/2.7bee3372.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todo-list/static/js/2.7bee3372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56192245758b81b811dc",
+    "url": "/Todo-list/static/js/main.d3129a45.chunk.js"
+  },
+  {
+    "revision": "90aaeb8dc8143d286822",
+    "url": "/Todo-list/static/js/runtime-main.b82090f8.js"
+  }
+]);
